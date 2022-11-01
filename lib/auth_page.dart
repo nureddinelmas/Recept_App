3937406742017f361a0bd11eb5258fee5822ff1b
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-11-01 13:26:18 +0100

    Editfields, logo, and fab

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -14,25 +14,111 @@ class _AuthPageState extends State<AuthPage> {
         MediaQuery.of(context).orientation == Orientation.landscape;
 
     return Scaffold(
-      backgroundColor: Colors.white70,
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          "Foodly",
+          style: TextStyle(fontFamily: "Times", fontSize: 35.0),
+        ),
       ),
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.1, 0.4, 0.6, 0.9],
-            colors: [
-              Colors.white10,
-              Colors.white12,
-              Colors.white10,
-              Colors.white10,
-            ],
+        height: 500,
+        color: Colors.grey,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  margin: const EdgeInsets.only(bottom: 10.0),
+                  padding: const EdgeInsets.only(top: 100),
+                  child: CircleAvatar(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                          "https://png.pngtree.com/png-vector/20190917/ourlarge/pngtree-meal-icon-vectors-png-image_1737729.jpg"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "Email",
+                      hintText: "",
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.mail),
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "Password",
+                      hintText: "",
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.visibility),
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                ),
+                GestureDetector(
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: Colors.white, fontFamily: "Times"),
+                  ),
+                ),
+                const Divider(
+                  thickness: 2,
+                  indent: 100,
+                  endIndent: 100,
+                  color: Colors.blueGrey,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 60.0),
+        child: SizedBox(
+          width: 200,
+          child: FloatingActionButton.extended(
+            onPressed: () {},
+            label: const Text("Log in"),
+            icon: const Icon(Icons.login),
+            backgroundColor: Colors.blueGrey,
+            extendedTextStyle: const TextStyle(fontFamily: "Times"),
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 
 class Main extends StatelessWidget {
   const Main({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
