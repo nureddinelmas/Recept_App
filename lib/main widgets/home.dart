6f58fb72ca6c664fa5e81c49aa397b68b10bd23d
nureddinelmas <nureddinelmas34@gmail.com>
2022-11-02 14:42:55 +0100

    recipe model added

--- lib/main widgets/home.dart
+++ lib/main widgets/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recept_app/minor%20widgets/fake_search.dart';
+import 'package:recept_app/my_home_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,7 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        title: const FakeSearch(),
+        title: const MyHomePage(),
       ),
     );
   }
