4d43d09dc571db708a70926aa75bfbdb985789c8
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2022-11-10 20:20:33 +0100

    PageView

--- lib/models/recipes.dart
+++ lib/models/recipes.dart
@@ -4,44 +4,37 @@ class Recipe {
   Recipe(
       {this.recipeName = '',
       this.recipeImage = '',
-      this.startColor,
-      this.endColor});
+      this.recipeDescription = ''});
 
   String recipeName;
   String recipeImage;
-  Color? startColor;
-  Color? endColor;
+  String recipeDescription;
 }
 
 var recipes = [
   Recipe(
-    recipeName: "Godaste fisken",
-    recipeImage: 'CostaBlanca.jpg',
-    startColor: const Color(0xFFF37552),
-    endColor: const Color(0xFFF58B5A),
-  ),
+      recipeName: "Godaste fisken",
+      recipeImage: 'CostaBlanca.jpg',
+      recipeDescription:
+          'Häll i fisken och massa vatten, låt koka i 200 minuter'),
   Recipe(
-    recipeName: "Godaste grytan",
-    recipeImage: 'Snake1.jpeg',
-    startColor: const Color(0xFF621e14),
-    endColor: const Color(0xFFd13b10),
-  ),
+      recipeName: "Godaste grytan",
+      recipeImage: 'Snake1.jpeg',
+      recipeDescription:
+          'Häll i alla ingredienser, rör runt ordentligt och krydda sen. Ät med ris'),
   Recipe(
-    recipeName: 'Godaste riset',
-    recipeImage: 'Snake2.jpeg',
-    startColor: const Color(0xFFe18b41),
-    endColor: const Color(0xFFc7c73d),
-  ),
+      recipeName: 'Godaste riset',
+      recipeImage: 'Snake2.jpeg',
+      recipeDescription:
+          'Stek riset, krydda med kryddor och koka sedan under lock'),
   Recipe(
-    recipeName: 'Godaste tacos',
-    recipeImage: 'SnakeTurquose.jpg',
-    startColor: const Color(0xFFF37552),
-    endColor: const Color(0xFFF58B5A),
-  ),
+      recipeName: 'Godaste tacos',
+      recipeImage: 'SnakeTurquose.jpg',
+      recipeDescription:
+          'Stek kött, lök och vitlök. Gör salsa verde och salsa roja och ha vid sidan av tillsammans med majstortillas'),
   Recipe(
-    recipeName: 'Godaste bönorna',
-    recipeImage: 'CostaBlanca.jpg',
-    startColor: const Color(0xFFF37552),
-    endColor: const Color(0xFFF58B5A),
-  ),
+      recipeName: 'Godaste bönorna',
+      recipeImage: 'CostaBlanca.jpg',
+      recipeDescription:
+          'Stek tillsammans med lök och lite vitlök. Mixa sedan med koriander, chili, paprika och ha i lite vatten'),
 ];
--- lib/screens/my_recipes.dart
+++ lib/screens/my_recipes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recept_app/colors.dart';
-import 'package:recept_app/widgets/pageview_my_recipes.dart';
+import 'package:recept_app/models/recipes.dart';
+import 'package:recept_app/widgets/my_recipe_card.dart';
 
 class MyRecipes extends StatefulWidget {
   const MyRecipes({Key? key}) : super(key: key);
@@ -12,73 +13,55 @@ class MyRecipes extends StatefulWidget {
 class _MyRecipesState extends State<MyRecipes> {
   int selectedIndex = 0;
 
+  final PageController _pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: mrBackgroundColor,
       body: SafeArea(
-        child: ListView(
-          primary: true,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(33),
-              child: Row(
-                children: [
-                  Center(
-                    child: Text(
-                      "My Recipes",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Expanded(
+                  child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    controller: _pageController,
+                    itemCount: recipes.length,
+                    itemBuilder: (context, index) => MyRecipeCard(index),
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            const PageViewMyRecipes(),
-            Container(
-              height: 270,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/images/CostaBlanca.jpg'),
-              )),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            const Text(
-              "Världens godaste mozzarellasallad tycker iallafall jag! Vad tycker ni egentligen?",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF0E0E2D),
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Container(
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(26.5),
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFFf6925c),
-                      Color(0xFFf37552),
-                    ]),
-              ),
-              child: Center(
-                child: Text(
-                  'Add new recipe',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ),
-            )
-          ],
-        ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(26.5),
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xFFf6925c),
+                              Color(0xFFf37552),
+                            ]),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Add new recipe',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
