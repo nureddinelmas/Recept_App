cd852ec6655bf7958d3ebc826538135c1aef4519
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2022-11-11 14:24:08 +0100

    multilined

--- lib/screens/add_recipe.dart
+++ lib/screens/add_recipe.dart
@@ -6,6 +6,10 @@ class AddRecipe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String recipeTitle = '';
+    String recipeIngredients = '';
+    String recipeDescription = '';
+
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
 
@@ -15,13 +19,14 @@ class AddRecipe extends StatelessWidget {
         title: Text(
           'Add recipe',
           style: TextStyle(
+            color: Colors.black,
             fontSize: 30,
             fontWeight: FontWeight.w600,
           ),
         ),
       ),
       body: Container(
-        height: height * 0.50,
+        height: height,
         width: width,
         child: Center(
           child: Container(
@@ -36,6 +41,13 @@ class AddRecipe extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(10),
                         child: TextField(
+                          showCursor: true,
+                          textAlign: TextAlign.center,
+                          textCapitalization: TextCapitalization.sentences,
+                          onChanged: (value) {
+                            recipeTitle = value;
+                            print(recipeTitle);
+                          },
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Colors.white,
@@ -56,7 +68,44 @@ class AddRecipe extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(10),
                         child: TextField(
+                          keyboardType: TextInputType.multiline,
                           maxLines: null,
+                          showCursor: true,
+                          textAlign: TextAlign.center,
+                          textCapitalization: TextCapitalization.sentences,
+                          onChanged: (value) {
+                            recipeIngredients = value;
+                            print(recipeIngredients);
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            icon: Icon(
+                              Icons.apple_rounded,
+                              color: Colors.amberAccent,
+                            ),
+                            hintText: 'Ingredients',
+                            hintStyle: TextStyle(color: mrBackgroundColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          showCursor: true,
+                          textAlign: TextAlign.center,
+                          textCapitalization: TextCapitalization.sentences,
+                          onChanged: (value) {
+                            recipeDescription = value;
+                            print(recipeDescription);
+                          },
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Colors.white,
@@ -64,7 +113,7 @@ class AddRecipe extends StatelessWidget {
                               Icons.book,
                               color: Colors.amberAccent,
                             ),
-                            hintText: 'Ingredients',
+                            hintText: 'Description',
                             hintStyle: TextStyle(color: mrBackgroundColor),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.all(
@@ -83,18 +132,33 @@ class AddRecipe extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                Container(
-                  height: height * 0.1,
-                  width: width * 0.85,
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  color: mrBackgroundColor,
-                  child: Expanded(
-                    child: TextField(
-                      expands: true,
-                      maxLines: null,
-                    ),
-                  ),
-                ),
+                // Container(
+                //   height: height * 0.1,
+                //   width: width * 0.85,
+                //   padding: const EdgeInsets.symmetric(vertical: 10.0),
+                //   color: mrBackgroundColor,
+                //   child: Expanded(
+                //     child: TextField(
+                //       decoration: InputDecoration(
+                //         filled: true,
+                //         fillColor: Colors.white,
+                //         icon: Icon(
+                //           Icons.book,
+                //           color: Colors.amberAccent,
+                //         ),
+                //         hintText: 'Ingredients',
+                //         hintStyle: TextStyle(color: mrBackgroundColor),
+                //         border: OutlineInputBorder(
+                //           borderRadius: BorderRadius.all(
+                //             Radius.circular(10),
+                //           ),
+                //         ),
+                //       ),
+                //       expands: true,
+                //       maxLines: null,
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
