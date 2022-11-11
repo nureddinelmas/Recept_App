821df3d323caf707727921bc192e1113e9d4ea4f
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2022-11-11 13:14:49 +0100

    decoratedTextFields

--- lib/screens/add_recipe.dart
+++ lib/screens/add_recipe.dart
@@ -6,6 +6,9 @@ class AddRecipe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mrBackgroundColor,
@@ -17,15 +20,96 @@ class AddRecipe extends StatelessWidget {
           ),
         ),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return SizedBox(
-              child: TextField(
-            expands: true,
-            maxLines: null,
-          ));
-        },
+      body: Container(
+        height: height * 0.50,
+        width: width,
+        child: Center(
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  color: mrGreenColor,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            icon: Icon(
+                              Icons.abc,
+                              color: Colors.amberAccent,
+                            ),
+                            hintText: 'Title',
+                            hintStyle: TextStyle(color: mrBackgroundColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                          maxLines: null,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            icon: Icon(
+                              Icons.book,
+                              color: Colors.amberAccent,
+                            ),
+                            hintText: 'Ingredients',
+                            hintStyle: TextStyle(color: mrBackgroundColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: height * 0.1,
+                  width: width * 0.85,
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  color: mrBackgroundColor,
+                  child: Expanded(
+                    child: TextField(
+                      expands: true,
+                      maxLines: null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+// LayoutBuilder(
+//         builder: (context, constraints) {
+//           SizedBox(
+//             height: constraints.maxHeight / 2,
+//             width: constraints.maxWidth / 0.8,
+//             child: TextField(
+//               expands: true,
+//              maxLines: null,
+//             ),
