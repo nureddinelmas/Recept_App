ce7471ef30c192c6b5a30c032cc59759df28ce90
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2022-11-11 11:51:45 +0100

    expanding textfielt added

--- lib/screens/add_recipe.dart
+++ lib/screens/add_recipe.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:recept_app/colors.dart';
 
 class AddRecipe extends StatelessWidget {
   const AddRecipe({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hej hej heeeeeej'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mrBackgroundColor,
+        title: Text(
+          'Add recipe',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SizedBox(
+              child: TextField(
+            expands: true,
+            maxLines: null,
+          ));
+        },
+      ),
     );
   }
 }
