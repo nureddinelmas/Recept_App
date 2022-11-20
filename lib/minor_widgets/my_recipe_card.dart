99b418d05a28a156138919ec18bc696550186100
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2022-11-20 21:06:39 +0100

    can show image

--- lib/minor_widgets/my_recipe_card.dart
+++ lib/minor_widgets/my_recipe_card.dart
@@ -8,33 +8,108 @@ class MyRecipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10.0),
-                      child: Text(_myRecipe.recipeTitle),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                gradient: LinearGradient(
+                    colors: [Colors.pink, Colors.red],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.red,
+                    blurRadius: 12,
+                    offset: Offset(0, 6),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: CustomPaint(
+                size: Size(100, 150),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Image.network(
+                  '${_myRecipe.image}',
+                  height: 64,
+                  width: 64,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _myRecipe.recipeTitle,
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w500),
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('Ingredients: ${_myRecipe.recipeIngredients}')
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('Description: ${_myRecipe.recipeDescription}')
+                    Text(
+                      'Ingredients: ${_myRecipe.recipeIngredients}',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      'Description: ${_myRecipe.recipeDescription}',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                    )
                   ],
                 )
               ],
-            )),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
+//  @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Card(
+//         child: Padding(
+//             padding: const EdgeInsets.all(12.0),
+//             child: Stack(
+//               children: [
+//                 Row(
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.only(bottom: 10.0),
+//                       child: Text(_myRecipe.recipeTitle),
+//                     ),
+//                   ],
+//                 ),
+//                 Row(
+//                   children: [
+//                     Text('Ingredients: ${_myRecipe.recipeIngredients}')
+//                   ],
+//                 ),
+//                 Row(
+//                   children: [
+//                     Text('Description: ${_myRecipe.recipeDescription}'),
+//                     Container(
+//                     child: Image.network('${_myRecipe.image}'),
+//                     ),
+//                     //   Image.network(
+//                     //   'https://picsum.photos/id/1074/400/400',
+//                     // ),
+//                   ],
+//                 )
+//               ],
+//             )),
+//       ),
+//     );
+//   }
