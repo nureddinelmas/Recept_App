7620ba0fe08cb9b297cb624410293c0b3c68243a
nureddinelmas <nureddinelmas34@gmail.com>
2022-11-22 08:41:31 +0100

    recipe Modal fixat

--- lib/models/recipeModel.dart
+++ lib/models/recipeModel.dart
@@ -508,14 +508,10 @@ Future<List> getRecipes(String q) async {
 
   for (var item in body) {
     list1.add(item['recipe']);
-
-    for (var element in list1) {
-      list2.add(element);
-    }
   }
-  list1.forEach((element) {
-    list2.add(element);
-  });
 
+  for (var element in list1) {
+    list2.add(element);
+  }
   return list2;
 }
